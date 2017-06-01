b3ac74fa7037b5b192cefc2c373d5156724d6989
Jens H. Nielsen <jens@jens.hk>
2017-06-01 13:39:43 +0800

    Added progress

--- js/weapon.js
+++ js/weapon.js
@@ -3,15 +3,25 @@ var Weapon = function(settings) {
   var handCursor = document.getElementsByClassName('arena')[0].style.cursor="url('hand.png'), none"
   var knifeCursor = document.getElementsByClassName('arena')[0].style.cursor="url('knife.png'), none"
 
-      $('.arena').on('click', function(event){
+  $('.arena').on('click', function(event){
 
-          if(handCursor){
-            var clicks = settings.handClick++;
-            if (clicks > 15)
-            $('#damage5').show();
-          }
+      if(handCursor){
+        var clicks = settings.handClick++;
+        if (clicks > 15){
+          $('#damage5').show();
+        }
 
-        });
+        if(clicks > 15){
+          clicks = 15;
+        }
+
+        var width = (100 / 15) * clicks
+        $('.progress-bar').css('width', width + '%');
+
+
+      }
+
+  });
 
 
     this.render = function(interactions,gameWeapon){
